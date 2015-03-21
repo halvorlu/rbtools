a4edf79cc773932264aefe530de5261daa6bb1af
Halvor Lund <halvor.lund@gmail.com>
2015-03-21 23:13:03 +0100

    Added a mercurial hook for posting review requests when pushing to central repo.

---
+++ rbtools/hooks/tests.py
@@ -0,0 +1,31 @@
+"""Tests for rbtools.hooks."""
+
+from rbtools.utils.testbase import RBTestBase
+from rbtools.hooks.common import find_ticket_refs, linkify_ticket_refs
+
+
+class CommonTest(RBTestBase):
+    """Tests for rbtools.hooks.common."""
+
+    def test_ticket_refs(self):
+        """Testing that ticket refs are interpreted as IDs correctly."""
+        self.assertEqual([10], find_ticket_refs("fixes #10"))
+        self.assertEqual([10, 11, 12],
+                         find_ticket_refs("fixes #10, and #11, and 12"))
+        self.assertEqual([10], find_ticket_refs("see ticket: #10"))
+        self.assertEqual([10], find_ticket_refs("addresses #10"))
+        self.assertEqual([1, 2],
+                         find_ticket_refs("fixes #1. addresses #2"))
+        self.assertEqual([10], find_ticket_refs("fixes bug 10"))
+        self.assertEqual([2, 10, 11],
+                         find_ticket_refs("see issue: 10,11,2"))
+        self.assertEqual([1, 2, 3, 4, 5],
+            find_ticket_refs("fixes 1, 2, 3. Addresses 3, 4, 5."))
+
+    def test_linkify_refs(self):
+        """Testing that references to tickets are linkified."""
+        url = "a/"
+        self.assertEqual("fixes [#10](a/10)",
+                         linkify_ticket_refs("fixes #10", url))
+        self.assertEqual("fixes [#10](a/10), [#11](a/11) and [#12](a/12)",
+                         linkify_ticket_refs("fixes #10, #11 and #12", url))
