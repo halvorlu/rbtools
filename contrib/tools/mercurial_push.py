9ea4da93ec047c044b87061d2bc8b5a0ecbff87f
Halvor Lund <halvor.lund@gmail.com>
2015-04-16 21:28:52 +0200

    Changes after review.
    - Use six.text_type instead of string
    - Single-quoted strings
    - Blank lines before/after indented blocks
    - Rearrange imports according to PEP8
    - %-formatting
    - Smarter string concatenation
    - and more

--- contrib/tools/mercurial_push.py
+++ contrib/tools/mercurial_push.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python2
 # coding: utf-8
-"""A Mercurial hook to post to ReviewBoard on push to a central server.
+"""A Mercurial hook to post to Review Board on push to a central server.
 
-The hook was designed to make posting to ReviewBoard easy for new or
-inexperienced users. It allows user to post to ReviewBoard by using the
+The hook was designed to make posting to Review Board easy for new or
+inexperienced users. It allows user to post to Review Board by using the
 ordinary "hg push", without any need to learn or install RBTools locally.
 
 This hook fits the following workflow:
@@ -46,7 +46,7 @@ The hook is configured through:
    section.
 
 In <repo>/.reviewboardrc, the following settings are relevant:
-REVIEWBOARD_URL: The URL of the ReviewBoard server
+REVIEWBOARD_URL: The URL of the Review Board server
 API_TOKEN: An API token to use for logging in, which allows submitting
            review requests on behalf of other users.
 
@@ -75,116 +75,129 @@ pretxnchangegroup.rb = /path/to/hook/mercurial_push.py
 """
 from __future__ import unicode_literals
 
-import rbtools.hooks.mercurial as hghook
-from rbtools.utils.filesystem import load_config
-from rbtools.api.errors import APIError
-import logging
-import getpass
 import itertools
+import getpass
+import logging
+
+import six
+
+import rbtools.hooks.mercurial as hghook
+from rbtools.api.errors import APIError
+from rbtools.utils.filesystem import load_config
 
 
-CONFIG_SECTION = "reviewboardhook"
+CONFIG_SECTION = 'reviewboardhook'
 
 
 def push_review_hook(node):
     """Run the hook. node is the hex of the first changeset."""
     config = load_config()
-    repo_root = hghook.extcmd(["hg", "root"]).strip()
+    repo_root = hghook.extcmd(['hg', 'root']).strip()
+
     try:
         root = hghook.get_root(config)
         rbrepo = hghook.get_repo(root, repo_root)
     except hghook.LoginError as error:
-        for line in str(error).split('\n'):
+        for line in six.text_type(error).split('\n'):
             logging.error(line)
         return hghook.HOOK_FAILED
+
     if 'REVIEWBOARD_URL' not in config:
-        logging.error("You need to specify REVIEWBOARD_URL in the repo's" +
-                      " .reviewboardrc file.")
+        logging.error('You need to specify REVIEWBOARD_URL in the '
+                      "repository's .reviewboardrc file.")
         return hghook.HOOK_FAILED
     else:
         url = config['REVIEWBOARD_URL']
+
     return push_review_hook_base(root, rbrepo, node, url,
                                  submitter=getpass.getuser())
 
 
 def get_ticket_url():
     """Return URL root to issue tracker. Warn if not specified in hgrc."""
-    ticket_url = hghook.hg_config(CONFIG_SECTION, "ticket_url",
-                                  default="")
-    if ticket_url == "":
-        repo_root = hghook.extcmd(["hg", "root"]).strip()
-        logging.warning("{0}/.hg/hgrc should specify"
-                        .format(repo_root))
-        logging.warning("the URL to the bug tracker as the ")
-        logging.warning("ticket_url setting in the [" +
-                        CONFIG_SECTION + "] section.")
-        logging.warning("Links to tickets/bugs in the review request summary")
-        logging.warning("or description may not work.")
+    ticket_url = hghook.hg_config(CONFIG_SECTION, 'ticket_url',
+                                  default='')
+    if not ticket_url:
+        repo_root = hghook.extcmd(['hg', 'root']).strip()
+        logging.warning('%s/.hg/hgrc should specify', repo_root)
+        logging.warning('the URL to the bug tracker as the ')
+        logging.warning('ticket_url setting in the [' +
+                        CONFIG_SECTION + '] section.')
+        logging.warning('Links to tickets/bugs in the review request summary')
+        logging.warning('or description may not work.')
+
     return ticket_url
 
 
 def get_ticket_prefixes():
     """Return a list of allowed prefixes in ticket IDs."""
-    prefixes = hghook.hg_config(CONFIG_SECTION, "ticket_id_prefixes",
+    prefixes = hghook.hg_config(CONFIG_SECTION, 'ticket_id_prefixes',
                                 default=None)
     if prefixes is None:
-        return [""]
+        return ['']
     else:
-        prefixes = [x.strip() for x in prefixes.split(",")]
-        prefixes.append("")
+        prefixes = [x.strip() for x in prefixes.split(',')]
+        prefixes.append('')
         return prefixes
 
 
 def push_review_hook_base(root, rbrepo, node, url, submitter):
-    """Run the hook with given API root, Reviewboard repo and changeset.
+    """Run the hook with given API root, Review Board repo and changeset.
 
-    url is the ReviewBoard server URL.
-    submitter is the user name of the user that is submitting."""
+    node is the commit ID of the first changeset in the push.
+    url is the Review Board server URL.
+    submitter is the user name of the user that is submitting.
+    """
     ticket_url = get_ticket_url()
     ticket_prefixes = get_ticket_prefixes()
     changesets = hghook.list_of_incoming(node)
-    parent = node + "^1"
-    logging.info("{0} changesets received.".format(len(changesets)))
+    parent = node + '^1'
+    logging.info('%d changesets received.', len(changesets))
     revreqs, indices, exact = hghook.find_review_requests(root, rbrepo,
                                                           changesets)
     exact_revreqs = list(itertools.compress(revreqs, exact))
     exact_indices = list(itertools.compress(indices, exact))
     last_approved_revreq = hghook.find_last_approved(exact_revreqs)
+
     if last_approved_revreq == -1:
         last_approved = -1
     else:
         last_approved = exact_indices[last_approved_revreq]
+
     not_approved = changesets[last_approved + 1:]
+
     if is_approved(not_approved):
         for revreq in revreqs:
-            logging.info("Closing review request: " + revreq.absolute_url)
+            logging.info('Closing review request: ' + revreq.absolute_url)
             hghook.close_request(revreq)
         return hghook.HOOK_SUCCESS
     else:
         if last_approved_revreq == len(revreqs) - 1:
             # All known review requests have been approved
-            logging.info("Creating review request for new changesets.")
+            logging.info('Creating review request for new changesets.')
             revreq = create(root, rbrepo, submitter, url, not_approved[-1])
             hghook.update_draft(root, ticket_url, ticket_prefixes,
                                 not_approved, revreq, parent=parent)
         else:
-            logging.info("Pending review request found.")
+            logging.info('Pending review request found.')
             revreq = revreqs[-1]
             add_to_pending(revreq, root, ticket_url, ticket_prefixes,
                            not_approved, parent)
         publish_maybe(revreq)
+
         if last_approved > -1:
-            logging.info("If you want to push the already approved changes,")
-            logging.info("you can (probably) do this by executing")
-            logging.info("'hg push -r {0}'"
-                         .format(changesets[last_approved]))
+            logging.info('If you want to push the already approved changes,')
+            logging.info('you can (probably) do this by executing')
+            logging.info('hg push -r %s', changesets[last_approved])
+
         return hghook.HOOK_FAILED
 
 
 def publish_maybe(revreq):
     """Publish the review request's draft if config says so."""
-    publish_draft = hghook.configbool("reviewboardhook", "publish",
+    publish_draft = hghook.configbool('reviewboardhook', 'publish',
                                       default=True)
+
     if publish_draft:
         revreq = revreq.get_self()  # Update request to get draft
         try:
@@ -196,24 +209,26 @@ def publish_maybe(revreq):
             if api_error.error_code != 100:
                 raise
     else:
-        logging.info("The review request has not been published yet.")
-        logging.info("You must publish it manually.")
+        logging.info('The review request has not been published yet.')
+        logging.info('You must publish it manually.')
 
 
 def is_approved(changesets):
     """Return True if the list of changesets are approved for pushing.
 
     Approval means that all changesets are merges, and that merges are allowed,
-    or that len(changesets) == 0."""
+    or that len(changesets) == 0.
+    """
     if len(changesets) == 0:
         return True
     else:
-        allow_merge = hghook.configbool("reviewboardhook", "allow_merge",
+        allow_merge = hghook.configbool('reviewboardhook', 'allow_merge',
                                         default=False)
-        if allow_merge and all([hghook.is_merge(ctx) for ctx in changesets]):
-            logging.info("New commits are merges, " +
-                         "which are automatically approved")
+        if allow_merge and all(hghook.is_merge(ctx) for ctx in changesets):
+            logging.info('New commits are merges, '
+                         'which are automatically approved')
             return True
+
     return False
 
 
@@ -221,12 +236,13 @@ def add_to_pending(revreq, root, ticket_url, ticket_prefixes,
                    changesets, parent):
     """Add any new changesets to draft of pending review request."""
     if revreq.extra_data['real_commit_id'] != changesets[-1]:
-        logging.info("Adding new commits to this review request.")
+        logging.info('Adding new commits to this review request.')
         hghook.update_draft(root, ticket_url, ticket_prefixes,
                             changesets, revreq, parent=parent)
-    logging.warning("Cannot push until this review request" +
-                    " is completed.")
-    logging.warning("URL: {0}".format(revreq.absolute_url))
+
+    logging.warning('Cannot push until this review request'
+                    ' is completed.')
+    logging.warning('URL: %s', revreq.absolute_url)
 
 
 def create(root, rbrepoid, submitter, url, changeset):
@@ -234,25 +250,27 @@ def create(root, rbrepoid, submitter, url, changeset):
     review_requests = root.get_review_requests(only_fields='',
                                                only_links='create')
     commit_id = hghook.date_author_hash(changeset)
+
     try:
         revreq = review_requests.create(commit_id=commit_id,
                                         repository=rbrepoid,
                                         submit_as=submitter)
     except APIError as api_error:
         if api_error.error_code == 208:
-            register_url = url + "account/register/"
-            logging.error("Could not create review request.")
-            logging.error("Make sure you have created a user named '" +
-                          submitter + "'.")
-            logging.error("Go to " + register_url + " to create a user.")
+            register_url = url + 'account/register/'
+            logging.error('Could not create review request.')
+            logging.error('Make sure you have created a user named ' +
+                          submitter + '.')
+            logging.error('Go to ' + register_url + ' to create a user.')
         raise api_error
-    logging.info("The review request must be completed before"
-                 " you can push again.")
-    logging.info("URL: {0}".format(revreq.absolute_url))
+
+    logging.info('The review request must be completed before'
+                 ' you can push again.')
+    logging.info('URL: %s', revreq.absolute_url)
     return revreq
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import sys
     import os
     logging.basicConfig(format='%(levelname)s: %(message)s',
--- rbtools/hooks/mercurial.py
+++ rbtools/hooks/mercurial.py
@@ -1,55 +1,61 @@
 """Generic functions for Mercurial hooks."""
 from __future__ import unicode_literals
 
+from datetime import datetime
+import hashlib
+import logging
+import os
+import subprocess
+
+import six
+
 from rbtools.api.client import RBClient
 from rbtools.api.errors import AuthorizationError, APIError
 from rbtools.clients.mercurial import MercurialClient
 from rbtools.hooks.common import linkify_ticket_refs, find_ticket_refs
-import logging
-import subprocess
-import os
-import hashlib
-from datetime import datetime
 
 
 HOOK_FAILED = True  # True means error (not equal to zero)
 HOOK_SUCCESS = False  # False means OK (equal to zero)
-DELIMITER = "\\(reviewboardhook will keep text below this line\\)\n"
+DELIMITER = '\\(reviewboardhook will keep text below this line\\)\n'
 
 
 def generate_summary(changesets):
     """Generate a summary from a list of changeset ids."""
-    summary = extcmd(["hg", "log", "-r", changesets[0], "--template",
-                      "{desc}"])
-    return summary.decode('utf-8').split("\n")[0]
+    summary = extcmd(['hg', 'log', '-r', changesets[0], '--template',
+                      '{desc}'])
+    return summary.decode('utf-8').split('\n')[0]
 
 
 def generate_description(changesets):
     """Generate a description from a list of changeset ids."""
-    header = "{0} changesets:\n".format(len(changesets))
-    maintext = extcmd(["hg", "log", "-r", changesets[0] + ":" + changesets[-1],
-                       "--template",
-                       "{author} ({date|isodate}):\n{desc}\n\n"])
+    header = '%d changesets:\n' % len(changesets)
+    maintext = extcmd(['hg', 'log', '-r', changesets[0] + ':' + changesets[-1],
+                       '--template',
+                       '{author} ({date|isodate}):\n{desc}\n\n'])
     return header + maintext.decode('utf-8')
 
 
 def join_descriptions(old_description, new_description):
     """Join two descriptions, keeping any changes after delimiter."""
     delim_index = old_description.find(DELIMITER)
+
     if delim_index == -1:
         keep = DELIMITER
     else:
         keep = old_description[delim_index:]
+
     return new_description + keep
 
 
 def upload_diff(root, changesets, revreq, parent):
     """Upload a diff for the given changesets to given review request."""
     differ = MercurialDiffer(root)
-    diff_info = differ.diff(changesets[0] + "^1",
+    diff_info = differ.diff(changesets[0] + '^1',
                             changesets[-1], parent)
     parent_diff = diff_info['parent_diff']
     diffs = revreq.get_diffs(only_links='upload_diff', only_fields='')
+
     if len(parent_diff) > 0:
         diffs.upload_diff(diff_info['diff'],
                           parent_diff=parent_diff,
@@ -60,8 +66,8 @@ def upload_diff(root, changesets, revreq, parent):
 
 def date_author_hash(changeset):
     """Return a MD5 hash (base64) of the author/date of given changeset."""
-    text = extcmd(["hg", "log", "-r", changeset,
-                   "--template", "{author} {date}"])
+    text = extcmd(['hg', 'log', '-r', changeset,
+                   '--template', '{author} {date}'])
     return hashlib.md5(text).hexdigest()
 
 
@@ -69,22 +75,26 @@ def update_draft(root, ticket_url, ticket_prefixes,
                  changesets, revreq, parent=None):
     """Update review request draft based on changesets.
 
-    parent is the last commit known by the repository before the push."""
+    parent is the last commit known by the repository before the push.
+    """
     old_description = revreq.description
     new_plain_description = generate_description(changesets)
     linked_description = linkify_ticket_refs(new_plain_description,
                                              ticket_url, ticket_prefixes)
     description = join_descriptions(old_description, linked_description)
     summary = generate_summary(changesets)
+
     if parent is None:
-        parent = changesets[0] + "^1"
+        parent = changesets[0] + '^1'
+
     upload_diff(root, changesets, revreq, parent)
-    string_refs = [str(x) for x in find_ticket_refs(new_plain_description)]
-    bugs_closed = ",".join(string_refs)
+    string_refs = [six.text_type(x)
+                   for x in find_ticket_refs(new_plain_description)]
+    bugs_closed = ','.join(string_refs)
     commit_id = date_author_hash(changesets[-1])
     extra_data = {'extra_data.real_commit_id': changesets[-1]}
     revreq.update(**extra_data)
-    branch = extcmd(["hg", "branch"]).strip()
+    branch = extcmd(['hg', 'branch']).strip()
     draft = revreq.get_draft(only_links='update', only_fields='')
     draft = draft.update(
         summary=summary,
@@ -112,10 +122,13 @@ class MercurialDiffer(object):
         diff_info['parent_diff'] is the parent diff (between base and rev1)
 
         parent is the last commit known before the push.
-        If parent is unspecified, it is assumed to be rev1."""
+        If parent is unspecified, it is assumed to be rev1.
+        """
         if parent is None:
             parent = rev1
-        revisions = {'base': rev1, 'tip': rev2, 'parent_base': parent}
+        revisions = {'base': rev1,
+                     'tip': rev2,
+                     'parent_base': parent}
         diff_info = self.tool.diff(revisions=revisions)
         return diff_info
 
@@ -131,7 +144,7 @@ def extcmd(cmd, cwd=None):
 
 
 class LoginError(Exception):
-    """Represents an error when logging in to ReviewBoard."""
+    """Represents an error when logging in to Review Board."""
     pass
 
 
@@ -148,9 +161,10 @@ class AlreadyExistsError(Exception):
 def hg_config(section, name, default=None):
     """Return the Mercurial config value with given section and name."""
     try:
-        result = extcmd(["hg", "showconfig", section + "." + name]).strip()
+        result = extcmd(['hg', 'showconfig', section + '.' + name]).strip()
     except subprocess.CalledProcessError:
         return default
+
     if len(result) > 0:
         return result
     else:
@@ -159,8 +173,8 @@ def hg_config(section, name, default=None):
 
 def configbool(section, name, default=False):
     """Return the Mercurial config boolean value section.name."""
-    text = hg_config(section, name, str(default))
-    if text == "0" or text == "False":
+    text = hg_config(section, name, six.text_type(default))
+    if text == '0' or text == 'False':
         return False
     else:
         return True
@@ -168,34 +182,34 @@ def configbool(section, name, default=False):
 
 def is_merge(commit):
     """Return True if given commit is a merge."""
-    res = extcmd(["hg", "log", "-r", "merge() and " + commit]).strip()
+    res = extcmd(['hg', 'log', '-r', 'merge() and ' + commit]).strip()
     return len(res) > 0
 
 
 def close_request(rev_req):
     """Close the given review request with a message."""
-    message = "Automatically closed by reviewboardhook (invoked by a push)."
-    rev_req.update(status="submitted",
-                   close_description=message)
+    message = 'Automatically closed by reviewboardhook (invoked by a push).'
+    rev_req.update(status='submitted', close_description=message)
 
 
 def datetime_from_timestamp(timestamp):
-    """Return a datetime object from a ReviewBoard timestamp."""
-    timestamp_format = "%Y-%m-%dT%H:%M:%SZ"
-    return datetime.strptime(timestamp, timestamp_format)
+    """Return a datetime object from a Review Board timestamp."""
+    return datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
 
 
 def get_last_diff_time(revreq):
     """Return datetime object of last diff upload to given review request."""
-    diffs = revreq.get_diffs(only_fields='timestamp')
-    datetimes = [datetime_from_timestamp(diff.timestamp) for diff in diffs]
-    return max(datetimes)
+    return max([
+        datetime_from_timestamp(diff.timestamp)
+        for diff in revreq.get_diffs(only_fields='timestamp')
+    ])
 
 
 def approved_by_others(revreq):
     """Return True if the review request was approved by someone else.
 
-    The approval must have been given after the last diff update."""
+    The approval must have been given after the last diff update.
+    """
     if not revreq.approved:
         return False
     diff_date = get_last_diff_time(revreq)
@@ -209,8 +223,8 @@ def approved_by_others(revreq):
                                       only_links='')
         user_id = review_user.id
         if review.ship_it:
-            logging.debug("Review request {0} has been approved by {1}"
-                          .format(revreq.id, review_user.username))
+            logging.debug('Review request %s has been approved by %s'
+                          % (revreq.id, review_user.username))
             review_time = datetime_from_timestamp(review.timestamp)
             if review_time < diff_date:
                 old_reviews.append(review_user.username)
@@ -218,13 +232,16 @@ def approved_by_others(revreq):
                 return True
             if user_id == revreq_user_id:
                 self_approved = True
+
     if len(old_reviews) > 0:
-        logging.info("Review request has been approved by " +
-                     ", ".join(old_reviews) + ",")
-        logging.info("but not after the last diff update.")
+        logging.info('Review request has been approved by ' +
+                     ', '.join(old_reviews) + ',')
+        logging.info('but not after the last diff update.')
+
     if self_approved:
-        logging.info("Review request has been approved by you,")
-        logging.info("but must also be approved by someone else.")
+        logging.info('Review request has been approved by you,')
+        logging.info('but must also be approved by someone else.')
+
     return False
 
 
@@ -236,10 +253,11 @@ def shorthex(longhex):
 def list_of_incoming(node):
     """Return a list of all changeset hexes after (and including) node.
 
-    Assumes that all incoming changeset have subsequent revision numbers."""
-    lines = extcmd(["hg", "log", "-r", node + ":",
-                    "--template", "{node|short}\n"])
-    return lines.split("\n")[:-1]
+    Assumes that all incoming changeset have subsequent revision numbers.
+    """
+    lines = extcmd(['hg', 'log', '-r', node + ':',
+                    '--template', '{node|short}\n'])
+    return lines.split('\n')[:-1]
 
 
 def find_review_request(root, rbrepo_id, changeset):
@@ -258,8 +276,8 @@ def find_review_request(root, rbrepo_id, changeset):
     if len(revreqs) > 0:
         return revreqs[0]
     else:
-        raise NotFoundError("Review request with commit ID" +
-                            "{0} not found".format(commit_id))
+        raise NotFoundError('Review request with commit ID'
+                            '%s not found' % commit_id)
 
 
 def find_review_requests(root, rbrepoid, changesets):
@@ -267,7 +285,8 @@ def find_review_requests(root, rbrepoid, changesets):
 
     Also returns a list indicating whether the match was exact or not.
     An exact match means that both date, author and commit ID of the changeset
-    match the review request."""
+    match the review request.
+    """
     revreqs = []
     indices = []
     exact = []
@@ -291,8 +310,8 @@ def find_last_approved(revreqs):
     last_approved = -1
     for i, revreq in enumerate(revreqs):
         if approved_by_others(revreq):
-            logging.info("Approved review request found: {0}"
-                         .format(revreq.absolute_url))
+            logging.info('Approved review request found: %s',
+                         revreq.absolute_url)
             last_approved = i
     return last_approved
 
@@ -304,9 +323,9 @@ def get_username(config):
         username = config['USERNAME']
     else:
         username = getpass.getuser()
-        logging.warning("You have not specified any username " +
-                        "in ~/.reviewboardrc")
-        logging.warning("Assuming '{0}' as username.".format(username))
+        logging.warning('You have not specified any username '
+                        'in ~/.reviewboardrc')
+        logging.warning('Assuming %s as username.', username)
     return username
 
 
@@ -317,44 +336,47 @@ def get_password_or_token(config):
     elif 'PASSWORD' in config:
         return config['PASSWORD'], None
     else:
-        raise LoginError("You need to specify either a password " +
-                         "or API token\n" +
-                         "for ReviewBoard in your .reviewboardrc file.")
+        raise LoginError('You need to specify either a password or API token\n'
+                         'for Review Board in your .reviewboardrc file.')
 
 
 def get_root(config):
     """Get API root object."""
     username = get_username(config)
     password, api_token = get_password_or_token(config)
+
     if 'REVIEWBOARD_URL' in config:
         url = config['REVIEWBOARD_URL']
     else:
-        raise LoginError("You need to specify REVIEWBOARD_URL in the repo's" +
-                         " .reviewboardrc file.")
+        raise LoginError('You need to specify REVIEWBOARD_URL in the '
+                         "repository's .reviewboardrc file.")
+
     if 'ENABLE_PROXY' in config:
         enable_proxy = config['ENABLE_PROXY']
     else:
         enable_proxy = True
+
     try:
         client = RBClient(url, username=username, password=password,
                           api_token=api_token,
                           disable_proxy=not enable_proxy)
         root = client.get_root()
     except AuthorizationError:
-        register_url = url + "account/register/"
-        raise LoginError("Login to ReviewBoard failed. \n" +
-                         "Please verify that you:\n" +
-                         "1. Have a ReviewBoard user named " + username +
-                         ".\nYou can create a user by visiting\n" +
-                         register_url + "\n" +
-                         "2. Have either a password or API token in " +
-                         "~/.reviewboardrc or the repo's .reviewboardrc.")
+        register_url = url + 'account/register/'
+        raise LoginError('Login to Review Board failed. \n'
+                         'Please verify that you:\n'
+                         '1. Have a Review Board user named %s'
+                         '.\nYou can create a user by visiting\n %s\n'
+                         '2. Have either a password or API token in '
+                         "~/.reviewboardrc or the repository's .reviewboardrc."
+                         % (username, register_url))
     except APIError as api_error:
         if api_error.http_status == 404:
-            raise LoginError("HTTP 404 error. Is the ReviewBoard URL\n" +
-                             "{0} correct?".format(url))
+            raise LoginError('HTTP 404 error. Is the Review Board URL\n'
+                             '%s correct?' % url)
         else:
             raise api_error
+
     return root
 
 
@@ -363,9 +385,9 @@ def get_repo(root, path):
     repos = root.get_repositories(path=path, only_fields='id',
                                   only_links='')
     if repos.num_items < 1:
-        raise LoginError("Could not open ReviewBoard repository for path\n" +
-                         "{0}\n".format(path) +
-                         "Do you have the permissions to access this" +
-                         " repository?\nAsk the administrator" +
-                         " to get permissions.")
+        raise LoginError('Could not open Review Board repository '
+                         'for path\n%s\n'
+                         'Do you have the permissions to access this '
+                         'repository?\nAsk the administrator '
+                         'to get permissions.' % path)
     return repos[0].id
